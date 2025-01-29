4b2de99e7080ab98ba834c79a33b91f64513fab1
devbruniverse <8217679+devbruniverse@users.noreply.github.com>
2025-01-29 19:15:24 -0300

    add positions mock data

---
+++ app/utils/positions.ts
@@ -0,0 +1,196 @@
+export const jobPositions = [
+  {
+    title: 'Senior React Engineer',
+    company: 'Nerdery',
+    through: null,
+    location: 'Remote, United States',
+    startDate: 'June 2024',
+    endDate: 'November 2024',
+    description:
+      'Worked on the Purina Petfinder project migrating legacy Wordpress and Gatsby applications to a modern NextJS stack.',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.'
+    ],
+    skills: [
+      'Tailwind',
+      'NextJS',
+      'GatsbyJS',
+      'TypeScript',
+      'docker',
+      'REST APIs',
+      'React',
+      'JavaScript',
+      'Git',
+      'HTML'
+    ]
+  },
+  {
+    title: 'Senior React Engineer',
+    company: 'Adeva',
+    through: null,
+    location: 'Remote, United States',
+    startDate: 'October 2022',
+    endDate: 'August 2023',
+    description:
+      'Develop and maintain auctions and internal user operations using Javascript, TypeScript, React, and React Query for feature implementation. Successfully delivered impactful features, enhancing fundraising efforts. Optimized internal user operations, boosting productivity.',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.'
+    ],
+    skills: [
+      'Tailwind',
+      'React Query',
+      'TypeScript',
+      'docker',
+      'REST APIs',
+      'React',
+      'JavaScript',
+      'Git',
+      'HTML'
+    ]
+  },
+  {
+    title: 'Senior Frontend Developer',
+    company: 'Lettuce Grow',
+    through: { name: 'Oowlish', href: 'https://www.oowlish.com' },
+    location: 'Remote, United States',
+    startDate: 'May 2021',
+    endDate: 'August 2022',
+    description:
+      'Senior Frontend Developer working with Gatsby, React, React Native, Redux, Tailwind, and Jest for an outsource client from US. Worked on systems responsible for dealing with internal operations and consumer-facing, such as inventory and orders management, developing the client E-commerce solution and mobile app',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.'
+    ],
+    skills: [
+      'Tailwind',
+      'docker',
+      'REST APIs',
+      'React Native',
+      'GatsbyJS',
+      'Redux',
+      'React',
+      'JavaScript',
+      'Git',
+      'HTML'
+    ]
+  },
+  {
+    id: 3,
+    title: 'Contractor Full-Stack Developer',
+    company: 'Ubistart',
+    location: 'Remote, Brazil',
+    startDate: 'March 2021',
+    endDate: 'May 2021',
+    description:
+      'Full-stack Developer working with WordPress, React, and PHP. Worked as a contractor responsible for integrating React into WordPress for an EdTech client. My main responsibilities included fully integrating React with WordPress REST API service while expanding its functionalities.',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.'
+    ],
+    skills: [
+      'docker',
+      'WordPress',
+      'PHP',
+      'MySQL',
+      'REST APIs',
+      'React',
+      'JavaScript',
+      'Git',
+      'HTML',
+      'SQL'
+    ]
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Softexpert',
+    location: 'Joinville, Brazil',
+    startDate: 'May 2019',
+    endDate: 'December 2020',
+    description:
+      'Frontend Engineer developing with React, Redux, PHP, Enzyme, and REST APIs.\n Responsibilities include working on the internal engineering team to develop new technology for the company that would be used for a variety of modules such as ECM, HDM, ERP, and others that comprise the product delivered by the company, training new developers who were joining the company and developing special products on-demand for the final client.',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.'
+    ],
+    skills: [
+      'docker',
+      'Enzyme',
+      'Redux',
+      'PHP',
+      'MySQL',
+      'REST',
+      'React',
+      'JavaScript',
+      'Git',
+      'HTML',
+      'SQL'
+    ]
+  },
+  {
+    title: 'Full-stack Developer',
+    company: 'Livid',
+    location: 'Santa Rosa, Brazil',
+    startDate: 'July 2014',
+    endDate: 'April 2019',
+    description:
+      'Full-stack Developer working with Drupal, jQuery, MySQL, and PHP. Worked as a Full-stack Developer developing E-commerce for Brazilian and North American clients. During this period I had the opportunity to work with different languages in projects such as Angular and VueJS.',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.'
+    ],
+    skills: [
+      'docker',
+      'Drupal',
+      'PHP',
+      'MySQL',
+      'Vue',
+      'Angular',
+      'REST',
+      'JavaScript',
+      'Git',
+      'HTML',
+      'SQL'
+    ]
+  },
+  {
+    title: 'Lead Full-stack Developer / UX/UI',
+    company: 'Divtec',
+    location: 'Santa Rosa, Brazil',
+    startDate: 'January 2012',
+    endDate: 'December 2013',
+    description:
+      'Lead Full-stack Developer working with Joomla, jQuery, MySQL, and PHP. Worked as a Full-stack Developer developing a variety of websites, such as E-commerce and Portfolios. During this period I was also responsible for designing and prototyping using a plethora of open-source tools like Gimp and Inkscape, while being responsible for leading a small team, prospecting customers and support.',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.'
+    ],
+    skills: [
+      'Joomla',
+      'jQuery',
+      'PHP',
+      'MySQL',
+      'JavaScript',
+      'Git',
+      'HTML',
+      'SQL',
+      'GIMP',
+      'Inkscape'
+    ]
+  }
+];
