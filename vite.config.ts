8949aa4b53dd4f4bf3d94b5e798b30cad96c6c19
devbruniverse <8217679+devbruniverse@users.noreply.github.com>
2025-01-30 10:28:38 -0300

    add env to build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   plugins: [react(), tailwindcss(), TanStackRouterVite(), tsconfigPaths()],
   build: {
     cssMinify: true,
